856302d707ca37833f12dfdb29ac71a70a243fe5
kiliann <wilian_kilian@hotmail.com>
2021-10-08 12:52:32 +0200

    version 1

--- js/dom.js
+++ js/dom.js
@@ -9,8 +9,9 @@ function ejecutar(){
     //getElementById() esta es la primer forma de buscar
     //let input = document.getElementById('iNombre');
     //console.log(input.value);
+    //
 
-
+    //recoge los datod de la etiqueta input no como el anterior quera por la id
     let nombre = document.getElementsByTagName('input');
     console.log(nombre);
 }
