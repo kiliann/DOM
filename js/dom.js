682bdcfe85833ac76e49464a546564550eb7829f
kiliann <wilian_kilian@hotmail.com>
2021-10-19 12:42:43 +0200

    Explicacion 2 de DOM

--- js/dom.js
+++ js/dom.js
@@ -13,5 +13,30 @@ function ejecutar(){
 
     //recoge los datod de la etiqueta input no como el anterior quera por la id
     let nombre = document.getElementsByTagName('input');
-    console.log(nombre);
+    console.log(nombre[2].value);
+
+    let p = document.querySelector('.c1');
+    console.log(p.outerHTML);
+    let spans = document.querySelectorAll('.c1 span');
+    console.log(spans);
+
+
+    spans[1].innerHTML = 'Viva la pepa';
+
+    //Modificar estilos
+    spans[0].style.color ='blue';
+    spans[1].style.fontsize = '20pt';
+    //let mueve = document.querySelector('.parrafodelizable')
+   //mueve.style.left = '+20px';
+
+    setInterval(mover, 100);
+
+}
+let posicion = 0;
+function mover(){
+
+    let mueve = document.querySelector('.parrafodelizable')
+    mueve.style.left = `${posicion}px`;
+    posicion +=1;
+    console.log('TRON')
 }
